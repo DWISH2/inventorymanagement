4bf9d04a6e0cc4d20313d225d97999051fb65ec2
DWISH2 <DWISH2@users.noreply.github.com>
2017-08-09 12:06:45 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inventory Management</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+
+
+<!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+
+	<img class="logoNoorlander" src="logoNoorlander.jpg" alt="logoNoorlander" height="127" width="250">
+
+	<div class="container">
+		<div class="row">
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Data & camera's</span>
+              			<p>Huidige vooraad camera's en data apparatuur.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="datacameras.php">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Alarm & Domotica</span>
+              			<p>Huidige vooraad alarm en domotica.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Elektra & Afmontage</span>
+              			<p>Huidige vooraad elektra en afmontage.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Elektra voormontage</span>
+              			<p>Huidige vooraad elektra voormontage.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Loodgieter</span>
+              			<p>Huidige vooraad loodgieters materiaal.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Rookgas</span>
+              			<p>Huidige vooraad rookgas materiaal.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Bouwmaterialen</span>
+              			<p>Huidige vooraad bouwmaterialen.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">CV & Onderhoud</span>
+              			<p>Huidige vooraad CV en onderhouds materiaal.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	<div class="col s12 m4">
+         		 <div class="card blue-grey darken-1">
+            		<div class="card-content white-text">
+              			<span class="card-title">Gereedschap</span>
+              			<p>Huidige vooraad gereedschap.</p>
+            		</div>
+            		<div class="card-action">
+              			<a href="#">Inzien</a>
+            		</div>
+          		</div>
+        	</div>
+        	
+      	</div>
+	</div>
+
+
+</body>
+</html>
