7323de5718ba2a3a489a03ea8d553547dafdb837
DWISH2 <DWISH2@users.noreply.github.com>
2017-08-09 12:07:31 +0200

    datacameras.php

---
+++ datacameras.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data & camera</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+
+
+<!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	  <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+
+	<img class="logoNoorlander" src="logoNoorlander.jpg" alt="logoNoorlander" height="127" width="250">
+	<div class="container">
+		<div class="row">
+			<div class="col s2 m4">
+				<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+			</div>
+			<div class="col s2 m4">
+				<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+			</div>
+		</div>
+	</div>
+	
+
+</body>
+</html>
